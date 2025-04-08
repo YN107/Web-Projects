7acc2d30205334c46761240e5c8cf8fee7385e88
Yahia Ashraf <152027321+YN107@users.noreply.github.com>
2025-04-08 17:53:40 +0300

    Add files via upload

---
+++ Apple HomePage/main.js
@@ -0,0 +1,10 @@
+let mnImg = document.querySelector('.mn-img');
+let bg = document.querySelector('.container');
+
+function phones(phone){
+    mnImg.src =  phone;
+}
+
+function colors(color){
+    bg.style.background = color;
+}
